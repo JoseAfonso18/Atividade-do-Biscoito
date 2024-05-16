deceab975513698b553c46ff0f43d74ea1472adc
José Afonso <141967767+JoseAfonso18@users.noreply.github.com>
2024-05-15 22:08:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import biscoito from './assets/biscoito.png';
+
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+    this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+
+    this.frases = ['Siga os bons e aprenda con eles', 'O Bom-senso vale mais que muito conhecimento','O Riso é a menor distacia entre duas pessoas','Deixe de çlado as preocupaçoes e seja feiz','Realize o obvil, pense no improvavel e conquiste o imposivel','Acredite em milagres mais nao dependa deles','A maior Barreira do sucesso é o medo do fracasso'];
+  }
+  quebrarBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = this.frases[numeroAleatorio];
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <img src={biscoito} className="img" alt="imagem do biscoito"/>
+        <Botao acao={this.quebrarBiscoito} nome="Abrir Biscoito"/>
+        <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
+
+}
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acao}>{this.props.nome}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
